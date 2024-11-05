4915e512f3f32a84ce09577bf3eccc75d6944add
Saharat Thongin <was.dew4546@gmail.com>
2024-11-05 17:14:56 +0700

    enhance(css): Spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -9,12 +9,14 @@ type TSpinnerProps = HTMLAttributes<HTMLDivElement> & {};
  */
 export default function Spinner({ className }: TSpinnerProps) {
   return (
-    <div className="flex items-center justify-center">
+    <div
+      className={cn(
+        "flex items-center justify-center p-2 text-primary",
+        className,
+      )}
+    >
       <div
-        className={cn(
-          "inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]",
-          className,
-        )}
+        className="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
         role="status"
       >
         <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
