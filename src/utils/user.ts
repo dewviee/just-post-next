8fdf4a916480fa4a67998c3309b74871e4a44ea9
Saharat Thongin <was.dew4546@gmail.com>
2024-11-15 05:50:32 +0700

    enhance(user-utils):change hard code string to const keys

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -2,7 +2,7 @@ import { LOCAL_STORAGE_KEYS } from "@/constants/localStorageKeys";
 import { getLocalStorageItem } from "./localStorage";
 
 export function getUserAccessToken() {
-  return `Bearer ${getLocalStorageItem("ACCESS_TOKEN")}`;
+  return `Bearer ${getLocalStorageItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN)}`;
 }
 
 export function removeUserAccessToken() {
