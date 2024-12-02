9ea0a007fe49a4ef29650d5293febbb48b2172c5
Saharat Thongin <was.dew4546@gmail.com>
2024-12-02 07:05:39 +0700

    enhance(Button): Content center-x and reduce opacity when disabled

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,12 +5,20 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
 };
 
-const Button = ({ className, onClick, children, ...props }: ButtonProps) => {
+const Button = ({
+  className,
+  disabled,
+  onClick,
+  children,
+  ...props
+}: ButtonProps) => {
   return (
     <button
       {...props}
+      disabled={disabled}
       className={cn(
-        "flex items-center gap-2 rounded-full bg-primary px-4 py-1 text-white hover:border-gray-500 hover:bg-primaryActive",
+        "flex items-center justify-center gap-2 rounded-full bg-primary px-4 py-1 text-white hover:border-gray-500 hover:bg-primaryActive",
+        disabled ? "opacity-70" : "",
         className,
       )}
       onClick={onClick}
